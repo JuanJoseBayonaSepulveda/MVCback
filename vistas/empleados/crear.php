58c25d7fabd81e4f84dd90ac443fa776c7935f5d
Juan Bayona <juanjosbase@gmail.com>
2021-10-18 22:15:28 -0500

    first commit

---
+++ vistas/empleados/crear.php
@@ -0,0 +1,29 @@
+<form action="" class="container w-50">
+    <h2>Crear persona</h2>
+    <!--#imput nombre-->
+    <div class="row">
+        <div class="col-12">
+            <label for="" class="form-label">Nombre</label>
+            <input type="text" class="form-control" id="nombre">
+        </div>
+    </div>
+
+    <!--imput correo-->
+    <div class="row">
+        <div class="col-12">
+            <label for="" class="form-label">Correo</label>
+            <input type="text" class="form-control" id="correo">
+        </div>
+    </div>
+
+    <!--#boton enviar-->
+    <div class="row">
+        <div class="col-12">
+           <input type="submit" class="btn btn-success btn-lg" value="Agregar Persona" id="">
+        </div>
+    </div>
+
+
+
+
+</form>
---
+++ vistas/empleados/inicio.php
@@ -0,0 +1,20 @@
+<!--/* Adaptar a nuestro proyecto*/-->
+
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">ID</th>
+      <th scope="col">Nombre</th>
+      <th scope="col">Correo</th>
+      <th scope="col">Acciones</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td> 1 </td>
+      <td>Mark</td>
+      <td>Otto@correo</td>
+      <td>Editar | Borrar</td>
+    </tr>
+  </tbody>
+</table>
